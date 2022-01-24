013db174859aa9b8265c1a32d4a3315b2c566c26
Ian Montgomery <ianarthurmontgomery@gmail.com>
2022-01-24 15:24:02 +0000

    -Car changes colour on package pickup

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
     bool hasPackage;
     [SerializeField] float destroyDelay = 0.5f;
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("Ow! Drive more carefully!");
@@ -16,12 +27,14 @@ public class Collision : MonoBehaviour
             {
                 Debug.Log("Package collected");
                 hasPackage = true;
+                spriteRenderer.color = hasPackageColor;
                 Object.Destroy(other.gameObject, destroyDelay);
             }
         else if (other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 
